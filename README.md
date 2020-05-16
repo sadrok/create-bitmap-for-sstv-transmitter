32a39dc6ba4b8a4f17b3224b8fd10a4d5a1e1d9c
Francois <sadrok.za@gmail.com>
2020-05-16 22:10:13 +0200

    did previous TODO
    Use click to accept an optional `filename` argument and
    optional `center-align` option.
    
    Allow specifying a file containing the text or get
    the text from user input. Added an center-align
    option

--- README.md
+++ README.md
@@ -9,7 +9,12 @@ The purpose of the script is to provide the C hex values to plug into
 a Arduino sketch that transmits the bitmap and potentially any other
 dynamic text using SSTV.
 
-## Usage
+## Installation
+Generally you only need to have `pipenv` installed and run the commands 
+in one of the two options below. The first time will take a short 
+while to setup the virtual env but it should be faster afterwards.
+
+## Usage - option 1
 1. run `pipenv shell` to setup the virtual environment. It should install
 the packages as specified in the `Pipfile.lock` file.
 
@@ -18,8 +23,5 @@ output a string that can be copy & pasted into the Arduino sketch.
 It should also display the bitmap using whichever application is associated
 with images.
 
-## TODO
-* Read the text from `stdin` instead of just the coded lines of text
-inside the script.
-
-  
+## Usage - option 2
+1. run `pipenv run main.py`
--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+import os
+import sys
+
+import click
 from PIL import Image
 
 FONT8x11 = {
@@ -62,35 +66,60 @@ FONT8x11 = {
     "Z": (0x00, 0x7E, 0x06, 0x0E, 0x0C, 0x18, 0x30, 0x70, 0x60, 0x7E, 0x00,),
 }
 
-STATIC_LINES = [
-    'BACAR6',
-    'ZS6SCR',
-    '//////',
-]
-
 CHARACTER_WIDTH = 8
 CHARACTER_HEIGHT = 11
-MAX_LINE_LENGTH = max(len(line) for line in STATIC_LINES)
-BITMAP_WIDTH = CHARACTER_WIDTH * MAX_LINE_LENGTH
-BITMAP_HEIGHT = CHARACTER_HEIGHT * len(STATIC_LINES)
 
 
-if __name__ == '__main__':
-    bytemap = [0x00 for _ in range(len(STATIC_LINES) * CHARACTER_HEIGHT)
-               for _ in range(MAX_LINE_LENGTH)]
+@click.command("main")
+@click.argument("filename", required=False)
+@click.option("--center-align", type=click.BOOL, default=False, is_flag=True)
+def run(filename=None, center_align=False):
 
-    for line, line_text in enumerate(STATIC_LINES):
+    if filename:
+        print(f"Reading lines from file: {filename}")
+        lines = open(filename, "r").readlines()
+        print(lines)
+    else:
+        print(
+            "Enter the lines below and terminate with end-of-file character on it's own line"
+            " (e.g. Ctrl-Z in Windows or Ctrl-D on linux"
+        )
+        lines = sys.stdin.readlines()
+
+    line_count = len(lines)
+    print(f"Generating bitmap for {line_count} lines")
+
+    lines = [l.strip() for l in lines]
+    max_line_length = max(len(line) for line in lines)
+    bitmap_width = CHARACTER_WIDTH * max_line_length
+    bitmap_height = CHARACTER_HEIGHT * line_count
+
+    lines = [l.upper() for l in lines]
+    if center_align:
+        lines = [l.center(max_line_length) for l in lines]
+
+    bytemap = [
+        0x00
+        for _ in range(line_count * CHARACTER_HEIGHT)
+        for _ in range(max_line_length)
+    ]
+
+    for line, line_text in enumerate(lines):
         for y in range(CHARACTER_HEIGHT):
             for c, char in enumerate(line_text):
-                pos = (line * MAX_LINE_LENGTH * CHARACTER_HEIGHT)
-                pos += (y * MAX_LINE_LENGTH)
+                pos = line * max_line_length * CHARACTER_HEIGHT
+                pos += y * max_line_length
                 pos += c
-                bytemap[pos] = FONT8x11.get(char, (0xff,)*CHARACTER_HEIGHT)[y]
+                bytemap[pos] = FONT8x11.get(char, (0xFF,) * CHARACTER_HEIGHT)[y]
 
     # Here we print out the "C" style code to be copied into the Arduino code
-    print(', '.join('0x{:02x}'.format(b) for b in bytemap))
+    print(", ".join("0x{:02x}".format(b) for b in bytemap))
 
     # This bit just displays the image to see if it looks right as a bitmap
-    image = Image.new('1', (BITMAP_WIDTH, BITMAP_HEIGHT))
+    image = Image.new("1", (bitmap_width, bitmap_height))
     image.putdata([(x & (0x80 >> b)) for x in bytemap for b in range(8)])
     image.show("The Image")
+
+
+if __name__ == "__main__":
+    run()
